f9cca2c9afb502ee832c9d76656db077062cb499
Krzysztof <krzyskin@wp.pl>
2018-06-19 10:19:42 +0200

    basic game with ajax

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 $(() => {
-    const cardColors = ["red", "red", "green", "green", "blue", "blue", "violet", "violet", "yellow", "yellow", "grey", "grey", "brown", "brown", "orange", "orange", "pink", "pink"];
+    /*let c=18;
+    let array = [];
+    let names = [];
+    for(let i=0;i<c;i++){
+        let cardA = `<div>aaaa</div>`;
+        array.push(cardA);
+    }
+
+    array = array.join("");
+    document.querySelector('body').innerHTML = array;*/
+    let images =[];
+    const cardImagesAll = ["img-0", "img-0","img-1", "img-1", "img-2", "img-2", "img-3", "img-3", "img-4", "img-4", "img-5", "img-5", "img-6", "img-6", "img-7", "img-7", "img-8", "img-8"];
+
+    console.log(images[0]);
+
+    urlApi = "https://dog.ceo/api/breeds/image/random/9";
+        $.ajax({
+            url: urlApi,
+            async:false,
+        }).done(function (res) {
+
+            for (let i = 0; i < res.message.length; i++) {
+                images.push(res.message[i]);
+            }
+
+
+        }).fail(function () {
+            console.log('fail');
+        });
 
     let allCards = document.querySelectorAll("div");
 
-    allCards = [...cardAll];
+    allCards = [...allCards];
 
     const startTime = new Date().getTime();
     let activeCard = "";
@@ -18,14 +46,10 @@ $(() => {
         }
         activeCard.classList.remove('hidden');
         if (activeCards.length === 0) {
-            console.log("1");
             activeCards[0] = activeCard;
             return;
         } else {
-            console.log("2");
-            allCards.forEach(card => {
-                card.removeEventListener('click', clickCard)
-            });
+            allCards.forEach(card => card.removeEventListener('click', clickCard));
             activeCards[1] = activeCard;
 
             setTimeout(function () {
@@ -33,15 +57,15 @@ $(() => {
                     console.log("wygrana");
                     activeCards.forEach(card => card.classList.add("off"));
                         gameResult++;
-                        allCards = cards.filter( card => !card.classList.contains("off"));
-                        console.log(gameResult);
-                        console.log(gamePairs);
+                    $('.off').css( "background-image", `` );
+                        allCards = allCards.filter( card => !card.classList.contains("off"));
+
 
                         if (gameResult == gamePairs) {
                             console.log("wygrana gra");
                             const endTime = new Date().getTime();
                             const gameTime = (endTime - startTime)/1000;
-                            alert(`udało sie!!! Twój czs to ${gameTime}`);
+                            alert(`udało sie!!! Twój czas to ${gameTime}`);
                             location.reload();
                         }
 
@@ -54,18 +78,29 @@ $(() => {
                 activeCard = "";
                 activeCards.length = 0;
 
-                cards.forEach(card => card.addEventListener('click', clickCard));
+                allCards.forEach(card => card.addEventListener('click', clickCard));
             }, 1000)
 
 
         }
     };
+
+    console.log(images[0]);
     const init = function () {
-        allCards.forEach(card => {
-            const position = Math.floor(Math.random() * cardColors.length);
-            card.classList.add(cardColors[position]);
-            cardColors.splice(position, 1);
-        })
+
+       allCards.forEach(card => {
+            card.classList.add("hidden");
+            const position = Math.floor(Math.random() * cardImagesAll.length);
+            card.classList.add(cardImagesAll[position]);
+            cardImagesAll.splice(position, 1);
+
+        });
+
+
+       for(let i=0;i<allCards.length/2;i++){
+           $(`div[class*='${i}']`).css('background-image', `url(${images[i]})`);
+          }
+
     };
     setTimeout(function () {
         allCards.forEach(card => {
